2e4ae9253a16a450d7d7127eecb8b84becbec046
Mihir Kulkarni <mikukulkarni@gmail.com>
2020-02-04 20:37:27 +0530

    Routing Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,15 @@ import { MatButtonModule } from '@angular/material/button';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+//services
+import { DishService } from './services/dish.service';
+//routing
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -22,13 +27,17 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
