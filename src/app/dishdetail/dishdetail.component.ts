ddc8e2a336ccaf20fd6373eea01e577ee3005d10
Mihir Kulkarni <mikukulkarni@gmail.com>
2020-03-01 13:07:30 +0530

    Animations Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,7 +6,8 @@ import { DishService} from "../services/dish.service";
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder , FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, flyInOut, expand } from '../animations/app.animations';
+
 
 @Component({
   selector: 'app-dishdetail',
@@ -14,17 +15,9 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   styleUrls: ['./dishdetail.component.scss'],
   
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility(),
+    flyInOut(),
+    expand()
   ]
 })
 
